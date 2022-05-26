1f37f91c84ca50afc0640f8f49309909c8ad2e52
HariNaveenKumar T <harinaveen984@gmail.com>
2022-05-26 21:10:25 +0530

    Alpha Commit

---
+++ src/main/java/com/hari/studentcrud/Website5Application.java
@@ -0,0 +1,13 @@
+package com.hari.studentcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Website5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Website5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hari/studentcrud/Website5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.studentcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Website5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
